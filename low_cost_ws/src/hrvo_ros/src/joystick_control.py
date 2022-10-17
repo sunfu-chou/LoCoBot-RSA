007af7f122e339a82009a059a04cbb78a0f51ba4
joe-hung <joehung.ee08@nycu.edu.tw>
2022-10-17 17:11:39 +0800

    add joystick_control.py

---
+++ low_cost_ws/src/hrvo_ros/src/joystick_control.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python2.7
+import rospy
+from sensor_msgs.msg import Joy
+from std_msgs.msg import *
+from geometry_msgs.msg import Twist
+
+class Base_controller:
+  def __init__(self):
+    self.node_name = rospy.get_name()
+    rospy.loginfo("[%s] Initializing " %(self.node_name))
+
+    self.joy_sub = rospy.Subscriber("/joy", Joy, self.JoyCallback, queue_size=1)
+    self.cmd_pub = rospy.Publisher("/cmd_vel_mux/input/teleop", Twist, queue_size=1)
+
+    self.twist_msg = Twist()
+    rate = rospy.Rate(10)
+    self.control_flag = True # enable joystick control
+
+    while not rospy.is_shutdown():
+      if self.control_flag == True:
+        self.cmd_pub.publish(self.twist_msg)
+      rate.sleep()
+
+  def JoyCallback(self, msg):
+    if msg.buttons[7] == 1:
+        if self.control_flag == False:
+          print "joystick mode"
+        self.control_flag = True
+    if msg.buttons[6] == 1:
+        if self.control_flag == True:
+          print "auto mode"
+        self.control_flag = False
+
+    if self.control_flag == True:
+        linear = msg.axes[1]
+        angular = msg.axes[3]
+        self.twist_msg.linear.x = linear*0.35
+        self.twist_msg.linear.y = 0
+        self.twist_msg.linear.z = 0
+       
+        self.twist_msg.angular.z = angular
+        self.twist_msg.angular.x = 0
+        self.twist_msg.angular.y = 0
+        #self.cmd_pub.publish(self.twist_msg)
+        rospy.loginfo("(%f, %f)", linear,angular)
+
+if __name__ == "__main__":
+  rospy.init_node("joystick_control",anonymous=False)
+  joystick = Base_controller()
+  rospy.spin()
+    

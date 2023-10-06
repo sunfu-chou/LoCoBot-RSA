7be85c8c773870aa97304c7c4fac4c708accf2f8
hchengwang <hchengwang@gmail.com>
2023-10-06 21:06:48 +0800

    update Quacker and test

--- low_cost_ws/src/rostest_example/include/rostest_example/test_Quacker.py
+++ low_cost_ws/src/rostest_example/include/rostest_example/test_Quacker.py
@@ -1,5 +1,6 @@
-from .Quacker import Quacker
 import pytest
+
+from .Quacker import Quacker
 
 # test get_quack_string function
 def test_get_quack_string():
@@ -8,4 +9,9 @@ def test_get_quack_string():
     # test the get_quack_string function
     assert quacker.get_quack_string(1) == "Quack!"
 
-
+# test rounded mean funcition
+def test_rounded_mean():
+    # initialize a Quacker object
+    quacker = Quacker()
+    # test the rounded_mean function
+    assert quacker.rounded_mean([1, 2, 3]) == 2

adbd11b1b79bb6d613a7afab99c82392a2250114
wellyowo <welly.ee11@nycu.edu.tw>
2023-10-12 21:50:17 +0800

    Update: change set ros master ros ip method for locobot

--- locobot/set_wfh_workspace_env.sh
+++ locobot/set_wfh_workspace_env.sh
@@ -3,7 +3,36 @@
 # load pyrobot env
 load_pyrobot_env
 # source WFH workspace and set_rospkg_path
-source ~/WFH_locobot/set_ip.sh 127.0.0.1 127.0.0.1
+if [ "$1" ]; then
+
+    echo "ROS MASRER $1"
+
+    export ROS_MASTER_URI=http://$1:11311
+
+else
+
+    echo "ROS MASRER 127.0.0.1"
+
+    export ROS_MASTER_URI=http://127.0.0.1:11311
+
+fi
+
+
+
+if [ "$2" ]; then
+
+    echo "ROS IP $2"
+
+    export ROS_IP=$2
+
+else
+
+    echo "ROS IP 127.0.0.1"
+
+    export ROS_IP=127.0.0.1
+
+fi
+
 #source ROS/catkin_ws/devel/setup.bash 
 #source  ROS/catkin_ws/devel_isolated/setup.bash
 source ~/WFH_locobot/set_rospackage_path.sh
